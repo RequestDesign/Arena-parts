dd1a49f89e54ec39340193b44c9f1416c5b5398a
Mother1teresa <129117416+Mother1teresa@users.noreply.github.com>
2024-12-17 17:59:22 +0300

    Страница Каталог оригинальный

--- script/index.js
+++ script/index.js
@@ -1,40 +1,101 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Swiper Initialization
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "auto",
-    spaceBetween: 13,
-    loop: false,
-    navigation: {
-      nextEl: null,
-      prevEl: null,
-    },
-    breakpoints: {
-      769: {
-        slidesPerView: "auto",
-        spaceBetween: 13,
+  const swiperContainer = document.querySelector(".mySwiper");
+  if (swiperContainer) {
+    var swiper = new Swiper(swiperContainer, {
+      slidesPerView: "auto",
+      spaceBetween: 13,
+      loop: false,
+      navigation: {
+        nextEl: null,
+        prevEl: null,
       },
-      0: {
-        slidesPerView: "auto",
-        spaceBetween: 10,
+      breakpoints: {
+        769: {
+          slidesPerView: "auto",
+          spaceBetween: 13,
+        },
+        0: {
+          slidesPerView: "auto",
+          spaceBetween: 10,
+        },
       },
-    },
-  });
+    });
+  } else {
+    console.log(
+      "Элемент .mySwiper не найден, Swiper не будет инициализирован."
+    );
+  }
 
-  // Search Button and Dropdown
+  const swiperCategory = document.querySelector(".mySwiperCategory");
+  if (swiperCategory) {
+    var swiper = new Swiper(swiperCategory, {
+      slidesPerView: "auto",
+      spaceBetween: 21,
+      loop: false,
+      navigation: {
+        nextEl: "#swiper-next_btn",
+        prevEl: null,
+      },
+      breakpoints: {
+        769: {
+          slidesPerView: "auto",
+          spaceBetween: 13,
+        },
+        0: {
+          slidesPerView: "auto",
+          spaceBetween: 10,
+        },
+      },
+    });
+
+    // Add event listeners to buttons after Swiper is initialized
+    const buttons = swiperCategory.querySelectorAll(".original-top_item");
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        // Remove the active class from all buttons
+        buttons.forEach((btn) => {
+          btn.classList.remove("original-top_item-active");
+        });
+        // Add the active class to the clicked button
+        button.classList.add("original-top_item-active");
+      });
+    });
+  } else {
+    console.log(
+      "Элемент .mySwiperCategory не найден, Swiper не будет инициализирован."
+    );
+  }
+
+
+
   const buttons = document.querySelectorAll(".btn-search");
   const dropdowns = document.querySelectorAll(".dropdowns");
+  if (buttons.length > 0 && dropdowns.length > 0) {
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        dropdowns.forEach((dropdown) => {
+          dropdown.style.display = "none";
+        });
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      dropdowns.forEach((dropdown) => {
-        dropdown.style.display = "none";
+        buttons.forEach((btn) => btn.classList.remove("btn-search_active"));
+
+        button.classList.add("btn-search_active");
+
+        const target = button.getAttribute("data-target");
+        const targetDropdown = document.getElementById(target);
+
+        if (targetDropdown) {
+          targetDropdown.style.display = "flex";
+        } else {
+          console.log(`Целевой элемент с ID ${target} не найден.`);
+        }
       });
-      buttons.forEach((btn) => btn.classList.remove("btn-search_active"));
-      button.classList.add("btn-search_active");
-      const target = button.getAttribute("data-target");
-      document.getElementById(target).style.display = "flex";
     });
-  });
+  } else {
+    console.log("Кнопки или дропдауны не инициализированы.");
+  }
+
+
 
   // Catalog Menu
   const catalogButton = document.getElementById("catalogButton");
@@ -53,20 +114,28 @@ document.addEventListener("DOMContentLoaded", () => {
       ? '<svg class="category-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 2L10 10M10 10L18 2M10 10L2 18M10 10L18 18" stroke="#1A5FBC" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg>'
       : '<svg class="category-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.03332 1.66666H4.44999C2.62499 1.66666 1.66666 2.625 1.66666 4.44166V6.025C1.66666 7.84166 2.62499 8.8 4.44166 8.8H6.02499C7.84166 8.8 8.79999 7.84166 8.79999 6.025V4.44166C8.80832 2.625 7.84999 1.66666 6.03332 1.66666Z" fill="#1A5FBC"/><path d="M15.5583 1.66666H13.975C12.1583 1.66666 11.2 2.625 11.2 4.44166V6.025C11.2 7.84166 12.1583 8.8 13.975 8.8H15.5583C17.375 8.8 18.3333 7.84166 18.3333 6.025V4.44166C18.3333 2.625 17.375 1.66666 15.5583 1.66666Z" fill="#1A5FBC"/><path d="M15.5583 11.1917H13.975C12.1583 11.1917 11.2 12.15 11.2 13.9667V15.55C11.2 17.3667 12.1583 18.325 13.975 18.325H15.5583C17.375 18.325 18.3333 17.3667 18.3333 15.55V13.9667C18.3333 12.15 17.375 11.1917 15.5583 11.1917Z" fill="#1A5FBC"/><path d="M6.03332 11.1917H4.44999C2.62499 11.1917 1.66666 12.15 1.66666 13.9667V15.55C1.66666 17.375 2.62499 18.3333 4.44166 18.3333H6.02499C7.84166 18.3333 8.79999 17.375 8.79999 15.5583V13.975C8.80832 12.15 7.84999 11.1917 6.03332 11.1917Z" fill="#1A5FBC"/></svg>';
   }
-
-  catalogButton.addEventListener("click", () => {
-    isCatalogOpen = !isCatalogOpen;
-    catalog.classList.toggle("visible");
-    updateIcon(isCatalogOpen);
-  });
-
-  closeButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      isCatalogOpen = false;
-      catalog.classList.remove("visible");
+  if (catalogButton && catalog) {
+    catalogButton.addEventListener("click", () => {
+      isCatalogOpen = !isCatalogOpen;
+      catalog.classList.toggle("visible");
       updateIcon(isCatalogOpen);
     });
-  });
+  } else {
+    console.log("Кнопка каталога или меню каталога не инициализирована..");
+  }
+  if (closeButtons.length > 0) {
+    closeButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        isCatalogOpen = false;
+        catalog.classList.remove("visible");
+        updateIcon(isCatalogOpen);
+      });
+    });
+  } else {
+    console.log("Кнопки закрытия не инициализирована.");
+  }
+
+
 
   const modals = {
     auth: document.getElementById("authModal"),
@@ -81,8 +150,22 @@ document.addEventListener("DOMContentLoaded", () => {
     orderSuccess: document.getElementById("orderSuccessModal"),
   };
 
+  // Проверка наличия модальных окон
+  Object.entries(modals).forEach(([key, modal]) => {
+    if (!modal) {
+      console
+        .log
+        //  не инициализирована.
+        ();
+    }
+  });
+
   function closeModals() {
-    Object.values(modals).forEach((modal) => (modal.style.display = "none"));
+    Object.values(modals).forEach((modal) => {
+      if (modal) {
+        modal.style.display = "none";
+      }
+    });
   }
 
   function openModal(modal) {
@@ -114,17 +197,16 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  // Обработчики событий для кнопок выбора способа оплаты
   ["paymentOption1", "paymentOption2", "paymentOption3"].forEach(
     (id, index) => {
       const button = document.getElementById(id);
       if (button) {
         button.addEventListener("click", () => {
-          // Закрыть блок с QR-кодом, если он открыт
           const qrCodeBlock = document.getElementById("qrCodeBlock");
-          qrCodeBlock.classList.add("hidden"); // Скрыть блок с QR-кодом
+          if (qrCodeBlock) {
+            qrCodeBlock.classList.add("hidden");
+          }
 
-          // Показать кнопку "получить QR-код"
           const optionButton = document.getElementById(`option${index + 1}`);
           if (optionButton) {
             optionButton.classList.remove("hidden");
@@ -133,34 +215,42 @@ document.addEventListener("DOMContentLoaded", () => {
           showAdditionalButton(`option${index + 1}`);
         });
       } else {
-        console.error(`Button with ID ${id} not found.`);
+        //  не инициализирована.
       }
     }
   );
 
-  // Обработчик для кнопки "получить QR-код"
-  document.getElementById("option1").addEventListener("click", () => {
-    const qrCodeBlock = document.getElementById("qrCodeBlock");
-    qrCodeBlock.classList.remove("hidden"); // Показываем блок с QR-кодом
+  const option1Button = document.getElementById("option1");
+  if (option1Button) {
+    option1Button.addEventListener("click", () => {
+      const qrCodeBlock = document.getElementById("qrCodeBlock");
+      if (qrCodeBlock) {
+        qrCodeBlock.classList.remove("hidden");
+      }
 
-    // Скрыть кнопку "получить QR-код"
-    const optionButton = document.getElementById("option1");
-    if (optionButton) {
-      optionButton.classList.add("hidden");
-    }
-  });
+      option1Button.classList.add("hidden");
+    });
+  } else {
+    // console.log("Кнопка с ID 'option1' не инициализирована.");
+    //  не инициализирована.
+  }
 
-  // Обработчик для кнопки "К предыдущему шагу"
-  document.getElementById("backButton").addEventListener("click", () => {
-    const qrCodeBlock = document.getElementById("qrCodeBlock");
-    qrCodeBlock.classList.add("hidden"); // Скрыть блок с QR-кодом
+  const backButton = document.getElementById("backButton");
+  if (backButton) {
+    backButton.addEventListener("click", () => {
+      const qrCodeBlock = document.getElementById("qrCodeBlock");
+      if (qrCodeBlock) {
+        qrCodeBlock.classList.add("hidden");
+      }
 
-    // Показать кнопку "получить QR-код"
-    const optionButton = document.getElementById("option1");
-    if (optionButton) {
-      optionButton.classList.remove("hidden");
-    }
-  });
+      if (option1Button) {
+        option1Button.classList.remove("hidden");
+      }
+    });
+  } else {
+    //  не инициализирована.
+    console.log("Кнопка с ID 'backButton' не инициализирована.");
+  }
 
   document.addEventListener("click", (event) => {
     const { target } = event;
@@ -211,29 +301,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
   window.addEventListener("click", function (event) {
     const paymentModal = modals.payment;
-    if (event.target === paymentModal) {
+    if (paymentModal && event.target === paymentModal) {
       closeModals();
     }
   });
-
-  const closeAuthButton = document.querySelector(".close-auth");
-  if (closeAuthButton) {
-    closeAuthButton.addEventListener("click", closeModals);
-  }
-
-  const paymentModalContent = document.querySelector(
-    "#paymentModal .modal-content"
-  );
-  if (paymentModalContent) {
-    paymentModalContent.addEventListener("click", (event) => {
-      event.stopPropagation();
-    });
-  }
-
-  // Изначально показываем первую кнопку
   showAdditionalButton("option1");
 
-  
+
+
+  const uploadButton = document.getElementById("uploadButton");
+  if (uploadButton) {
+    uploadButton.addEventListener("click", () => {
+      const fileInput = document.getElementById("fileInput");
+      if (fileInput) {
+        fileInput.click();
+      }
+    });
+  } else {
+    //  не инициализирована.
+  }
+
+
+
+  const fileInput = document.getElementById("fileInput");
+  if (fileInput) {
+    fileInput.addEventListener("change", (event) => {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          const img = document.createElement("img");
+          img.src = e.target.result;
+          img.alt = "Загруженное изображение";
+          document.body.appendChild(img);
+        };
+        reader.readAsDataURL(file);
+      }
+    });
+  } else {
+    //  не инициализирована.
+  }
+
+
 
   const counters = document.querySelectorAll(".orders-product_block-counter");
   counters.forEach((counter) => {
@@ -242,26 +351,34 @@ document.addEventListener("DOMContentLoaded", () => {
     const increaseButton = counter.querySelector(".increase-button");
     let productCount = 1;
 
-    function updateProductButtonState() {
-      decreaseButton.classList.toggle("orders-circle_active", productCount > 1);
-    }
-
-    updateProductButtonState();
-
-    increaseButton.addEventListener("click", () => {
-      productCount++;
-      orderCountElement.textContent = productCount;
+    if (orderCountElement && decreaseButton && increaseButton) {
+      function updateProductButtonState() {
+        decreaseButton.classList.toggle(
+          "orders-circle_active",
+          productCount > 1
+        );
+      }
       updateProductButtonState();
-    });
 
-    decreaseButton.addEventListener("click", () => {
-      if (productCount > 1) {
-        productCount--;
+      increaseButton.addEventListener("click", () => {
+        productCount++;
         orderCountElement.textContent = productCount;
         updateProductButtonState();
-      }
-    });
+      });
+
+      decreaseButton.addEventListener("click", () => {
+        if (productCount > 1) {
+          productCount--;
+          orderCountElement.textContent = productCount;
+          updateProductButtonState();
+        }
+      });
+    } else {
+      console.log();
+    }
   });
+
+
 
   document.querySelectorAll(".details-top_categories-btn").forEach((group) => {
     const buttonsCatalog = group.querySelectorAll(".catalog-btn");
@@ -272,7 +389,6 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   });
-
   function toggleActive(activeBtn, buttonsInGroup) {
     buttonsInGroup.forEach((button) => {
       if (button === activeBtn) {
@@ -283,29 +399,37 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+
+
   const customSelects = document.querySelectorAll(".custom-select");
   customSelects.forEach((select) => {
     const selectedOption = select.querySelector(".view-block_select");
     const selectItems = select.querySelector(".select-items");
 
-    selectedOption.addEventListener("click", () => {
-      selectItems.classList.toggle("select-show");
-    });
+    if (selectedOption && selectItems) {
+      selectedOption.addEventListener("click", () => {
+        selectItems.classList.toggle("select-show");
+      });
 
-    window.addEventListener("click", (event) => {
-      if (!select.contains(event.target)) {
-        selectItems.classList.remove("select-show");
-      }
-    });
+      window.addEventListener("click", (event) => {
+        if (!select.contains(event.target)) {
+          selectItems.classList.remove("select-show");
+        }
+      });
+    } else {
+    }
   });
-
   window.selectOption = function (item, selectedId) {
     const selectedOption = document.getElementById(selectedId);
-    selectedOption.innerHTML = `${item.innerHTML} ${getSVGIcon()}`;
-    const selectItems = item.closest(".select-items");
-    selectItems.classList.remove("select-show");
+    if (selectedOption) {
+      selectedOption.innerHTML = `${item.innerHTML} ${getSVGIcon()}`;
+      const selectItems = item.closest(".select-items");
+      if (selectItems) {
+        selectItems.classList.remove("select-show");
+      }
+    } else {
+    }
   };
-
   function getSVGIcon() {
     return `
       <svg class="select-icon" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -314,7 +438,18 @@ document.addEventListener("DOMContentLoaded", () => {
     `;
   }
 
-  ymaps.ready(init);
+
+  
+  ymaps.ready(function () {
+    var mapElement = document.getElementById("map");
+    if (mapElement) {
+      init();
+    } else {
+      console.log(
+        "Элемент с ID 'map' не найден. Карта не будет инициализирована."
+      );
+    }
+  });
 
   function init() {
     var myMap = new ymaps.Map("map", {
@@ -322,6 +457,7 @@ document.addEventListener("DOMContentLoaded", () => {
       zoom: 16,
       controls: ["zoomControl", "typeSelector"],
     });
+
     var myPlacemark = new ymaps.Placemark(
       [55.7282, 37.6915],
       {},
@@ -341,15 +477,21 @@ document.addEventListener("DOMContentLoaded", () => {
     myMap.container.getElement().appendChild(addressDiv);
 
     function updateAddressPosition() {
-      var coords = myPlacemark.geometry.getCoordinates();
-      var pixelCoords = myMap.projection.toGlobalPixels(
-        coords,
-        myMap.getZoom()
-      );
-      addressDiv.style.left = pixelCoords[0] + "px";
-      addressDiv.style.top = pixelCoords[1] - 30 + "px";
+      if (myMap.projection) {
+        var coords = myPlacemark.geometry.getCoordinates();
+        var pixelCoords = myMap.projection.toGlobalPixels(
+          coords,
+          myMap.getZoom()
+        );
+        addressDiv.style.left = pixelCoords[0] + "px";
+        addressDiv.style.top = pixelCoords[1] - 30 + "px";
+      } else {
+        console.log("myMap.projection не определен.");
+      }
     }
+
     updateAddressPosition();
+
     myPlacemark.events.add("drag", updateAddressPosition);
     myPlacemark.events.add("dragend", updateAddressPosition);
   }
